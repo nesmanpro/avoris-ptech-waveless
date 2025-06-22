337cd1426f07740b86b5ea70d411e9ede0c8d187
nesmanpro <nesmanpro@gmail.com>
2025-06-22 18:37:11 +0200

    media queries 2

--- app/components/initModal.js
+++ app/components/initModal.js
@@ -34,27 +34,20 @@ export function renderSummaryModal(cardElement, cardData) {
     modal.style.top = '0px';
     modal.style.left = '0px';
 
-    if (window.innerWidth < 601) {
 
-        modal.style.top = 0;
-        modal.style.left = 0;
-        modal.style.maxWidth = '100%';
-
-    } else {
-
-        if (left + modalRect.width > window.innerWidth) {
-            left = rect.left - 135;
-            if (left < 0) left = 0;
-        }
-
-
-        if (top + modalRect.height > window.innerHeight) {
-            top = rect.top - 30;
-            if (top < 0) top = 0;
-        }
-        modal.style.top = `${top}px`;
-        modal.style.left = `${left}px`;
+    if (left + modalRect.width > window.innerWidth) {
+        left = rect.left - 135;
+        if (left < 0) left = 0;
     }
+
+
+    if (top + modalRect.height > window.innerHeight) {
+        top = rect.top - 30;
+        if (top < 0) top = 0;
+    }
+    modal.style.top = `${top}px`;
+    modal.style.left = `${left}px`;
+
 
     scaleUp(modal)
 
